6832bc5cca9dbb80b4ac380ae9b753f1ef78558e
Eli Cusic <tntitans31@gmail.com>
2020-06-04 21:57:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var updateObjectWithKeyAndValue = function(object, key, value){
+  var newObj = object
+  newObj[key] = value
+  return newObj
+}
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
+  object[key] = value
+  return object
+}
+
+deleteFromObjectByKey = function(object, key){
+  var newObj = object
+  delete newObj[key]
+  return newObj
+}
+
+destructivelyDeleteFromObjectByKey = function(object, key){
+  delete object.key
+  return object
+}
